590e9f082128ec554958fe1f2e007ba609172d36
Daniel Miranda <danielkza2@gmail.com>
2014-08-11 15:47:35 -0300

    zfs-prereqs: atualiza p/ Ubuntu

--- zfs_prerequisites.sh
+++ zfs_prerequisites.sh
@@ -1,39 +1,59 @@
 #!/bin/bash
 
 set -e
-cd /root
 
 # Add ZFS repo
 
+os_codename=$(lsb_release -s -c)
+
+case "$os_codename" in
+wheezy) ;;
+trusty) ;;
+*)
+    echo "Error: Unsupported OS codename ${release}" >&2
+    exit 1
+esac 
+
 mirror="$1"
-[ -n "$mirror" ] || mirror='http://debian.c3sl.ufpr.br/debian'
-
-wget -N 'http://archive.zfsonlinux.org/debian/pool/main/z/zfsonlinux/zfsonlinux_2%7Ewheezy_all.deb'
-dpkg -i zfsonlinux_2~wheezy_all.deb
-
-# Add backports for udev
-if ! [ -f /etc/apt/sources.list.d/wheezy-backports.list ]; then
-    cat > /etc/apt/sources.list.d/wheezy-backports.list <<EOF
-deb ${mirror} wheezy-backports main contrib non-free
-deb-src ${mirror} wheezy-backports main contrib non-free
-EOF
-fi
 
 export DEBIAN_FRONTEND=noninteractive
 
-apt-get update
+case "$os_codename" in)
+wheezy)
+    if [[ -z "$mirror" ]]; then
+        mirror='http://cdn.debian.net/debian'
+    fi
+
+    wget -N 'http://archive.zfsonlinux.org/debian/pool/main/z/zfsonlinux/zfsonlinux_2%7Ewheezy_all.deb' \
+     -O /tmp/zfsonlinux.deb
+    dpkg -i /tmp/zfsonlinux.deb
+    
+    # Add backports for udev
+    if ! [ -f /etc/apt/sources.list.d/wheezy-backports.list ]; then
+        cat > /etc/apt/sources.list.d/wheezy-backports.list <<EOF
+deb ${mirror} wheezy-backports main contrib non-free
+deb-src ${mirror} wheezy-backports main contrib non-free
+EOF
+    fi
+
+    apt-get update
+    apt-get install -y -t wheezy-backports udev
+
+    udevadm trigger
+    udevadm settle
+;;
+trusty)
+    apt-add-repository -y ppa:zfs-native/stable
+    apt-get update
+esac
 
 apt-get install -y linux-{image,headers}-amd64 \
  mdadm gdisk dosfstools e2fsprogs
 
-# /dev/disk/by-partuuid is broken in Wheezy. Install udev from backports to fix
-# that.
-
-apt-get install -y -t wheezy-backports udev
-
-udevadm trigger
-
-apt-get install -y debian-zfs
+case "$os_release" in
+wheezy) apt-get install -y debian-zfs ;;
+trusty) apt-get install -y ubuntu-zfs ;;
+esac
 
 # Check ZFS
 
