fa002acf4661c29aad963f7da0a023a5f4a3cc0d
Daniel Miranda <danielkza2@gmail.com>
2016-02-18 20:40:25 -0200

    Update example_create.sh

--- example_create.sh
+++ example_create.sh
@@ -4,11 +4,11 @@ set -e
 
 src_dir=$(readlink -f $(dirname "${BASH_SOURCE[0]}"))
 
-mkdir -p /mnt/dota
+mkdir -p /mnt/example
 
 b=/dev/disk/by-id
 "${src_dir}/zfs_create.sh" \
-  -n dota -m /mnt/dota -e 256M -b 256M -w 1024M -l 1024M \
+  -n dota -m /mnt/example -e 256M -b 256M -w 1024M -l 1024M \
   -d $b/wwn-0x6c81f660db7624001a82f0de0f68e96b \
   -d $b/wwn-0x6c81f660db7624001a82f0f710de325c \
   -d $b/wwn-0x6c81f660db7624001a82f10711d5e9a5 \
